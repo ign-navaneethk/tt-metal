f060e944ad78c125231e7475996ec4cd9bf8ebef
Navaneeth Krishnan <navaneeth.krishnan@ignitarium.com>
2025-07-30 17:06:52 +0530

    Adds functional bottleneck layer tests

---
+++ models/experimental/panoptic_deeplab/tests/test_resnet52_stem.py
@@ -0,0 +1,141 @@
+# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+import torch
+from loguru import logger
+from ttnn.model_preprocessing import preprocess_model_parameters
+
+import ttnn
+from tests.ttnn.utils_for_testing import check_with_pcc
+from models.experimental.panoptic_deeplab.reference.resnet52_stem import DeepLabStem
+from models.experimental.panoptic_deeplab.tt.stem import resnet52Stem
+from models.experimental.panoptic_deeplab.tt.custom_preprocessing import create_custom_mesh_preprocessor
+
+
+class Resnet52StemTestInfra:
+    def __init__(
+        self,
+        device,
+        batch_size,
+        inplanes,
+        planes,
+        height,
+        width,
+        stride,
+        model_config
+    ):
+        super().__init__()
+        torch.manual_seed(0)
+        self.pcc_passed = False
+        self.pcc_message = "Did you forget to call validate()?"
+        self.device = device
+        self.num_devices = device.get_num_devices()
+        self.batch_size = batch_size
+        self.inputs_mesh_mapper, self.weights_mesh_mapper, self.output_mesh_composer = self.get_mesh_mappers(device)
+
+        torch_model = DeepLabStem(
+            in_channels=inplanes,
+            out_channels=planes,
+            stride=stride,
+        ).eval()
+
+        input_shape = (batch_size * self.num_devices, inplanes, height, width)
+        self.torch_input_tensor = torch.rand(input_shape, dtype=torch.float32)
+
+        parameters = preprocess_model_parameters(
+            initialize_model=lambda: torch_model,
+            custom_preprocessor=create_custom_mesh_preprocessor(self.weights_mesh_mapper),
+            device=None,
+        )
+        torch_model.to(torch.bfloat16)
+        self.torch_input_tensor = self.torch_input_tensor.to(torch.bfloat16)
+
+        ## golden
+        self.torch_output_tensor = torch_model(self.torch_input_tensor)
+        ## ttnn
+        tt_host_tensor = ttnn.from_torch(
+            self.torch_input_tensor.permute(0, 2, 3, 1),
+            dtype=ttnn.bfloat16,
+            # device=device,
+            mesh_mapper=self.inputs_mesh_mapper,
+        )
+        self.ttnn_model = resnet52Stem(
+            parameters=parameters,
+            stride=stride,
+            model_config=model_config,
+        )
+        self.input_tensor = ttnn.to_device(tt_host_tensor, device)
+        self.run()
+        self.validate()
+
+    def get_mesh_mappers(self, device):
+        if device.get_num_devices() != 1:
+            inputs_mesh_mapper = ttnn.ShardTensorToMesh(device, dim=0)
+            weights_mesh_mapper = None  # ttnn.ReplicateTensorToMesh(device) causes unnecessary replication/takes more time on the first pass
+            output_mesh_composer = ttnn.ConcatMeshToTensor(device, dim=0)
+        else:
+            inputs_mesh_mapper = None
+            weights_mesh_mapper = None
+            output_mesh_composer = None
+        return inputs_mesh_mapper, weights_mesh_mapper, output_mesh_composer
+
+    def run(self):
+        self.output_tensor = self.ttnn_model(
+            self.input_tensor,
+            self.device,
+        )
+        return self.output_tensor
+
+    def validate(self, output_tensor=None):
+        output_tensor = self.output_tensor if output_tensor is None else output_tensor
+        output_tensor = ttnn.to_torch(output_tensor, device=self.device, mesh_composer=self.output_mesh_composer)
+        expected_shape = self.torch_output_tensor.shape
+        output_tensor = torch.reshape(output_tensor, (expected_shape[0], expected_shape[2], expected_shape[3], expected_shape[1]))
+        output_tensor = torch.permute(output_tensor, (0, 3, 1, 2))
+
+        batch_size = output_tensor.shape[0]
+
+        valid_pcc = 0.999
+        self.pcc_passed, self.pcc_message = check_with_pcc(self.torch_output_tensor, output_tensor, pcc=valid_pcc)
+
+        assert self.pcc_passed, logger.error(f"PCC check failed: {self.pcc_message}")
+        logger.info(
+            f"ResNet52 Stem Block batch_size={batch_size}, act_dtype={model_config['ACTIVATIONS_DTYPE']}, weight_dtype={model_config['WEIGHTS_DTYPE']}, math_fidelity={model_config['MATH_FIDELITY']}, PCC={self.pcc_message}"
+        )
+
+        return self.pcc_passed, self.pcc_message
+    
+model_config = {
+    "MATH_FIDELITY": ttnn.MathFidelity.LoFi,
+    "WEIGHTS_DTYPE": ttnn.bfloat8_b,
+    "ACTIVATIONS_DTYPE": ttnn.bfloat8_b,
+}
+
+@pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
+@pytest.mark.parametrize(
+    "batch_size, inplanes, planes, height, width, stride",
+    (
+        (1, 3, 128,  1024, 2048, 1), # Pass
+    ),
+)
+def test_stem(
+    device,
+    batch_size,
+    inplanes,
+    planes,
+    height,
+    width,
+    stride,
+):
+    Resnet52StemTestInfra(
+        device,
+        batch_size,
+        inplanes,
+        planes,
+        height,
+        width,
+        stride,
+        model_config,
+    )
