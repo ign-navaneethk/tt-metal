f8dae42d47d79a0a86cbafc05ef11b4afa1105e8
ign-akshayr <akshay.ram@ignitarium.com>
2025-08-21 21:48:05 +0530

    rn52 backbone implement

--- models/experimental/panoptic_deeplab/tt/bottleneck.py
+++ models/experimental/panoptic_deeplab/tt/bottleneck.py
@@ -7,10 +7,11 @@ from loguru import logger
 import ttnn
 from models.experimental.panoptic_deeplab.tt.common import TTConv2D
 
+
 class TTBottleneck:
     expansion: int = 4
 
-    def __init__(self, parameters, downsample, stride, model_config, dilation: int=1) -> None:
+    def __init__(self, parameters, downsample, stride, model_config, dilation: int = 1) -> None:
         self.conv1 = TTConv2D(
             kernel_size=1,
             stride=1,
@@ -20,7 +21,7 @@ class TTBottleneck:
             activation="relu",
             act_block_h=32,
             memory_config=ttnn.DRAM_MEMORY_CONFIG,
-            is_reshape=False, # not working
+            is_reshape=False,  # not working
         )
         self.conv2 = TTConv2D(
             kernel_size=3,
@@ -52,10 +53,7 @@ class TTBottleneck:
             slice_config = None
             if parameters.downsample.weight.shape[0] == 512 and parameters.downsample.weight.shape[1] == 256:
                 shard_layout = ttnn.TensorMemoryLayout.HEIGHT_SHARDED
-                slice_config = ttnn.Conv2dSliceConfig(  
-                    slice_type=ttnn.Conv2dSliceHeight,
-                    num_slices=4
-                )
+                slice_config = ttnn.Conv2dSliceConfig(slice_type=ttnn.Conv2dSliceHeight, num_slices=4)
             self.downsample_conv = TTConv2D(
                 kernel_size=1,
                 stride=stride,
@@ -72,20 +70,6 @@ class TTBottleneck:
         self.model_config = model_config
         return
 
-    def run_downsample_if_req(
-        self,
-        x,
-        device,
-    ):
-        if self.downsample:
-            logger.debug(f"Running downsample")
-            ds_out, _ = self.downsample_conv(device, x, x.shape)
-        else:
-            ds_out = x
-        ds_out = ttnn.reallocate(ds_out)
-        ttnn.deallocate(x)
-        return ds_out
-
     def __call__(
         self,
         x,
@@ -94,15 +78,16 @@ class TTBottleneck:
     ):
         # conv1 is 1x1 conv
         logger.debug(f"Running conv1")
-        out, shape = self.conv1(device, x, x.shape)
-    
+        out1, shape = self.conv1(device, x, x.shape)
+
         # conv2 is 3x3 conv
         logger.debug(f"Running conv2")
-        out, shape = self.conv2(device, out, shape)
-    
+        out2, shape = self.conv2(device, out1, shape)
+        ttnn.deallocate(out1)
         # conv3 is 1x1 conv
         logger.debug(f"Running conv3")
-        out, shape = self.conv3(device, out, shape)
+        out, shape = self.conv3(device, out2, shape)
+        ttnn.deallocate(out2)
 
         # run downsample conv 1x1 if required
         if self.downsample:
@@ -111,14 +96,16 @@ class TTBottleneck:
         else:
             ds_out = x
         ds_out = ttnn.reallocate(ds_out)
-    
+        ttnn.deallocate(x)
         if ds_out.shape != out.shape:
-            ds_out = ttnn.reshape(ds_out, (1, 1, ds_out.shape[0]*ds_out.shape[1]*ds_out.shape[2], ds_out.shape[3]))
+            ds_out = ttnn.reshape(ds_out, (1, 1, ds_out.shape[0] * ds_out.shape[1] * ds_out.shape[2], ds_out.shape[3]))
         if ds_out.layout != out.layout:
             ds_out = ttnn.to_layout(ds_out, out.layout)
         if ds_out.memory_config() != out.memory_config():
             ds_out = ttnn.to_memory_config(ds_out, out.memory_config())
 
+        out = ttnn.to_memory_config(out, ttnn.DRAM_MEMORY_CONFIG)
+        out = ttnn.reallocate(out)
         if eltwise_binary_out_in_place:
             # underscore version is in_place = True
             out = ttnn.add_(
@@ -133,6 +120,7 @@ class TTBottleneck:
                 activations=[ttnn.UnaryWithParam(ttnn.UnaryOpType.RELU)],
                 memory_config=ttnn.L1_MEMORY_CONFIG,
             )
+        out = ttnn.reshape(out, shape)
 
         ttnn.deallocate(ds_out)
         return out
