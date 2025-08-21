a29f041eab1d5c2080f2abddaace9f27450b935f
ign-akshayr <akshay.ram@ignitarium.com>
2025-08-21 21:48:05 +0530

    rn52 backbone implement

--- models/experimental/panoptic_deeplab/tests/test_resnet52_backbone.py
+++ models/experimental/panoptic_deeplab/tests/test_resnet52_backbone.py
@@ -1,39 +1,39 @@
-# SPDX-FileCopyrightText: © 2024 Tenstorrent Inc.
+# SPDX-FileCopyrightText: © 2025 Tenstorrent Inc.
 
 # SPDX-License-Identifier: Apache-2.0
 
 import pytest
 import torch
 from loguru import logger
-from ttnn.model_preprocessing import preprocess_model_parameters
-
 import ttnn
+from ttnn.model_preprocessing import preprocess_model_parameters
 from tests.ttnn.utils_for_testing import check_with_pcc
-from models.experimental.panoptic_deeplab.reference.resnet52_stem import DeepLabStem
-from models.experimental.panoptic_deeplab.tt.stem import resnet52Stem
+from models.experimental.panoptic_deeplab.reference.resnet52_backbone import ResNet52BackBone as TorchBackbone
+from models.experimental.panoptic_deeplab.tt.backbone import TTBackbone
 from models.experimental.panoptic_deeplab.tt.custom_preprocessing import create_custom_mesh_preprocessor
 
 
-class Resnet52StemTestInfra:
-    def __init__(self, device, batch_size, inplanes, planes, height, width, stride, model_config):
+class BackboneTestInfra:
+    def __init__(
+        self,
+        device,
+        batch_size,
+        in_channels,
+        height,
+        width,
+        model_config,
+    ):
         super().__init__()
         torch.manual_seed(0)
         self.pcc_passed = False
         self.pcc_message = "Did you forget to call validate()?"
         self.device = device
         self.num_devices = device.get_num_devices()
-        self.batch_size = batch_size
         self.inputs_mesh_mapper, self.weights_mesh_mapper, self.output_mesh_composer = self.get_mesh_mappers(device)
 
-        torch_model = DeepLabStem(
-            in_channels=inplanes,
-            out_channels=planes,
-            stride=stride,
-        ).eval()
-
-        input_shape = (batch_size * self.num_devices, inplanes, height, width)
+        torch_model = TorchBackbone().eval()
+        input_shape = (batch_size * self.num_devices, in_channels, height, width)
         self.torch_input_tensor = torch.rand(input_shape, dtype=torch.float32)
-
         parameters = preprocess_model_parameters(
             initialize_model=lambda: torch_model,
             custom_preprocessor=create_custom_mesh_preprocessor(self.weights_mesh_mapper),
@@ -41,19 +41,16 @@ class Resnet52StemTestInfra:
         )
         torch_model.to(torch.bfloat16)
         self.torch_input_tensor = self.torch_input_tensor.to(torch.bfloat16)
-
-        ## golden
         self.torch_output_tensor = torch_model(self.torch_input_tensor)
 
-        ## ttnn
         tt_host_tensor = ttnn.from_torch(
             self.torch_input_tensor.permute(0, 2, 3, 1),
             dtype=ttnn.bfloat16,
             mesh_mapper=self.inputs_mesh_mapper,
         )
-        self.ttnn_model = resnet52Stem(
+
+        self.ttnn_model = TTBackbone(
             parameters=parameters,
-            stride=stride,
             model_config=model_config,
         )
 
@@ -96,12 +93,12 @@ class Resnet52StemTestInfra:
 
         batch_size = output_tensor.shape[0]
 
-        valid_pcc = 0.999
+        valid_pcc = 0.99
         self.pcc_passed, self.pcc_message = check_with_pcc(self.torch_output_tensor, output_tensor, pcc=valid_pcc)
 
         assert self.pcc_passed, logger.error(f"PCC check failed: {self.pcc_message}")
         logger.info(
-            f"ResNet52 Stem Block batch_size={batch_size}, act_dtype={model_config['ACTIVATIONS_DTYPE']}, weight_dtype={model_config['WEIGHTS_DTYPE']}, math_fidelity={model_config['MATH_FIDELITY']}, PCC={self.pcc_message}"
+            f"ResNet52 Backbone batch_size={batch_size}, act_dtype={model_config['ACTIVATIONS_DTYPE']}, weight_dtype={model_config['WEIGHTS_DTYPE']}, math_fidelity={model_config['MATH_FIDELITY']}, PCC={self.pcc_message}"
         )
 
         return self.pcc_passed, self.pcc_message
@@ -116,25 +113,23 @@ model_config = {
 
 @pytest.mark.parametrize("device_params", [{"l1_small_size": 24576}], indirect=True)
 @pytest.mark.parametrize(
-    "batch_size, inplanes, planes, height, width, stride",
-    ((1, 3, 128, 1024, 2048, 1),),  # Pass
+    "batch_size, in_channels, height, width",
+    [
+        (1, 3, 1024, 2048),
+    ],
 )
-def test_stem(
+def test_backbone(
     device,
     batch_size,
-    inplanes,
-    planes,
+    in_channels,
     height,
     width,
-    stride,
 ):
-    Resnet52StemTestInfra(
+    BackboneTestInfra(
         device,
         batch_size,
-        inplanes,
-        planes,
+        in_channels,
         height,
         width,
-        stride,
         model_config,
     )
