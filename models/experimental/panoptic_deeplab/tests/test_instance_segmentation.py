e445d48ee1c654e9da0d8ecfcaa6eb28c30c9d47
ign-saurav <sauravkumar.mishra@ignitarium.com>
2025-09-02 21:47:54 +0000

    ins @80k us

--- models/experimental/panoptic_deeplab/tests/test_instance_segmentation.py
+++ models/experimental/panoptic_deeplab/tests/test_instance_segmentation.py
@@ -332,9 +332,9 @@ model_config = {
         # (1, "aspp"),                    # Test ASPP component only
         # (1, "res3"),                    # Test Res3 decoder only
         # (1, "res2"),                    # Test Res2 decoder only
-        (1, "center_head"),  # Test center head
+        # (1, "center_head"),  # Test center head
         # (1, "offset_head"),                   # Test offset head
-        # (1, "full"),                      # Test full instance segmentation block
+        (1, "full"),  # Test full instance segmentation block
     ],
 )
 def test_modular_panoptic_deeplab_instance_segmentation(
